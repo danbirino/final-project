464a4d2c5fec201bf1ba04c6b37b74498d781289
danbirino <danbirino@gmail.com>
2018-07-26 19:56:15 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,4 +5,38 @@ $(function(){
     const $midName = $('#m-name');
     const $firstName = $('#f-name');
 
+    $( "#l-name" )
+    .mouseover(function() {
+        $("#l-name").css("color","#003334");
+    })
+    .mouseout(function() {
+        $("#l-name").css("color","#d4d4d4")
+    })
+    .click(function() {
+        window.location = "./lee.html";
+    });
+
+    $( "#m-name" )
+    .mouseover(function() {
+        $("#m-name").css("color","#003334");
+    })
+    .mouseout(function() {
+        $("#m-name").css("color","#d4d4d4")
+    })
+    .click(function() {
+        window.location = "./dan.html";
+    });
+
+    $( "#f-name" )
+    .mouseover(function() {
+        $("#f-name").css("color","#003334");
+    })
+    .mouseout(function() {
+        $("#f-name").css("color","#d4d4d4")
+    })
+    .click(function() {
+        window.location = "./bi.html";
+    });
+      
+
 })
